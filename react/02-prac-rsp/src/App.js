21e213872b3a65e424db263e668abbbc63ee67e0
mi <167054762+myungju524@users.noreply.github.com>
2024-07-02 17:06:10 +0900

    000

--- react/02-prac-rsp/src/App.js
+++ react/02-prac-rsp/src/App.js
@@ -1,14 +1,10 @@
 import { useState } from "react";
 import "./App.css";
+import HandIcon from "./HandIcon";
 import "./HandIcon.css";
 import reset from "./assets/ic-reset.svg";
-import HandIcon from "./HandIcon";
 import HandButton from "./HandButton";
 import { compareHand, generateRandomHand } from "./utils";
-
-function random(n) {
-  return Math.ceil(Math.random() * n);
-}
 
 function getResult(comparison) {
   if (comparison > 0) return "승리";
@@ -20,27 +16,23 @@ function App() {
   const [hand, setHand] = useState("rock");
   const [otherHand, setOtherHand] = useState("rock");
   const [score, setScore] = useState(0);
-  const [otherScore, setOtherScore] = useState(0);
+  const [otherScore, SetOtherScore] = useState(0);
   const [bet, setBet] = useState(1);
   const [gameHistory, setGameHistory] = useState([]);
   const [isWin, setIsWin] = useState(0);
 
   const handleButtonClick = (value) => {
-    // 사용자가 클릭한 주먹가위보 가져와야한다.
     setHand(value);
-    // 상대의 주먹가위보 랜덤으로 추출
     const nextOtherHand = generateRandomHand();
     setOtherHand(nextOtherHand);
-    // 승패 결정 ==> 배점을 곱해서 점수 추출
+
     const comparison = compareHand(value, nextOtherHand);
     setIsWin(comparison);
     if (comparison > 0) setScore(score + bet);
-    if (comparison < 0) setOtherScore(otherScore + bet);
-    // 결정된 승패를 state에 저장
+    if (comparison < 0) SetOtherScore(otherScore + bet);
     const result = getResult(comparison);
     setGameHistory([...gameHistory, result]);
   };
-
   const handleBetChange = (e) => {
     let num = Number(e.target.value);
     if (num > 9) num %= 10;
@@ -48,12 +40,11 @@ function App() {
     num = Math.floor(num);
     setBet(num);
   };
-
-  const handleClearClick = () => {
+  const handClearClick = () => {
     setHand("rock");
     setOtherHand("rock");
     setScore(0);
-    setOtherScore(0);
+    SetOtherScore(0);
     setBet(1);
     setGameHistory([]);
     setIsWin(0);
@@ -61,7 +52,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="App-heading">가위바위보</h1>
-      <img className="App-reset" src={reset} onClick={handleClearClick} />
+      <img className="App-reset" src={reset} onClick={handClearClick} />
       <div className="App-scores">
         <div className="Score">
           <div className="Score-num">{score}</div>
@@ -74,14 +65,11 @@ function App() {
         </div>
       </div>
       <div className="Box App-box">
-        {/* 중괄호 열면 자바스크립트 영역 */}
-        {/* 가위바위보 내는 것 */}
         <div className="App-hands">
           <div
             className={`Hand ${isWin == 0 ? "" : isWin == 1 ? "Winner" : ""}`}
           >
             <HandIcon value={hand} className="Hand-icon" />
-            {/* 여기에 쓴 className은 prop의 역할 */}
           </div>
           <div className="App-versus">VS</div>
           <div
@@ -90,7 +78,6 @@ function App() {
             <HandIcon value={otherHand} className="Hand-icon" />
           </div>
         </div>
-        {/* 배점 */}
         <div className="App-bet">
           <span>배점</span>
           <input
@@ -102,7 +89,6 @@ function App() {
           />
           <span>배</span>
         </div>
-        {/* 기록 */}
         <div className="App-history">
           <h2>승부기록</h2>
           <p>{gameHistory.join(", ")}</p>
--- react/02-prac-rsp/src/utils.js
+++ react/02-prac-rsp/src/utils.js
@@ -5,7 +5,6 @@ const WINS = {
 };
 
 export function compareHand(a, b) {
-  // 승리 ==> 1, 패배 ==> -1,  무승부 ==> 0
   if (WINS[a] === b) return 1;
   if (WINS[b] === a) return -1;
   return 0;
@@ -14,7 +13,6 @@ export function compareHand(a, b) {
 function random(n) {
   return Math.ceil(Math.random() * n);
 }
-
 export function generateRandomHand() {
   const num = random(3);
   if (num === 1) {
