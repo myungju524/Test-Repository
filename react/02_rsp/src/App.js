29b5b15a3f4db544bd9229bd4680075f45817cba
myungju524 <hmj970524@gmail.com>
2024-07-03 22:31:45 +0900

    dd

--- react/02_rsp/src/App.js
+++ react/02_rsp/src/App.js
@@ -1,117 +1,36 @@
 import { useState } from "react";
 import "./App.css";
-import "./HandIcon.css";
 import reset from "./assets/ic-reset.svg";
-import HandIcon from "./HandIcon";
-import HandButton from "./HandButton";
-import { compareHand, generateRandomHand } from "./utils";
-
+import myGame from "./assets/rock.svg";
+import otherGame from "./assets/rock.svg";
+import gameZone from "./GameZone";
 function random(n) {
   return Math.ceil(Math.random() * n);
 }
 
-function getResult(comparison) {
-  if (comparison > 0) return "승리";
-  if (comparison < 0) return "패배";
-  return "무승부";
-}
-
 function App() {
-  const [hand, setHand] = useState("rock");
-  const [otherHand, setOtherHand] = useState("rock");
-  const [score, setScore] = useState(0);
-  const [otherScore, setOtherScore] = useState(0);
-  const [bet, setBet] = useState(1);
-  const [gameHistory, setGameHistory] = useState([]);
-  const [isWin, setIsWin] = useState(0);
+  const [myScore, setMyScore] = useState(1);
+  const [otherScore, setOtherScore] = useState(1);
 
-  const handleButtonClick = (value) => {
-    // 사용자가 클릭한 주먹가위보 가져와야한다.
-    setHand(value);
-    // 상대의 주먹가위보 랜덤으로 추출
-    const nextOtherHand = generateRandomHand();
-    setOtherHand(nextOtherHand);
-    // 승패 결정 ==> 배점을 곱해서 점수 추출
-    const comparison = compareHand(value, nextOtherHand);
-    setIsWin(comparison);
-    if (comparison > 0) setScore(score + bet);
-    if (comparison < 0) setOtherScore(otherScore + bet);
-    // 결정된 승패를 state에 저장
-    const result = getResult(comparison);
-    setGameHistory([...gameHistory, result]);
-  };
-
-  const handleBetChange = (e) => {
-    let num = Number(e.target.value);
-    if (num > 9) num %= 10;
-    if (num < 1) num = 1;
-    num = Math.floor(num);
-    setBet(num);
-  };
-
-  const handleClearClick = () => {
-    setHand("rock");
-    setOtherHand("rock");
-    setScore(0);
-    setOtherScore(0);
-    setBet(1);
-    setGameHistory([]);
-    setIsWin(0);
-  };
   return (
     <div className="App">
-      <h1 className="App-heading">가위바위보</h1>
-      <img className="App-reset" src={reset} onClick={handleClearClick} />
-      <div className="App-scores">
-        <div className="Score">
-          <div className="Score-num">{score}</div>
-          <div className="Score-name">나</div>
-        </div>
-        <div className="App-versus">:</div>
-        <div className="Score">
-          <div className="Score-num">{otherScore}</div>
-          <div className="Score-name">상대</div>
-        </div>
-      </div>
-      <div className="Box App-box">
-        {/* 중괄호 열면 자바스크립트 영역 */}
-        {/* 가위바위보 내는 것 */}
-        <div className="App-hands">
-          <div
-            className={`Hand ${isWin == 0 ? "" : isWin == 1 ? "Winner" : ""}`}
-          >
-            <HandIcon value={hand} className="Hand-icon" />
-            {/* 여기에 쓴 className은 prop의 역할 */}
-          </div>
-          <div className="App-versus">VS</div>
-          <div
-            className={`Hand ${isWin == 0 ? "" : isWin == 1 ? "" : "Winner"}`}
-          >
-            <HandIcon value={otherHand} className="Hand-icon" />
-          </div>
-        </div>
-        {/* 배점 */}
-        <div className="App-bet">
-          <span>배점</span>
-          <input
-            type="number"
-            min={1}
-            max={9}
-            value={bet}
-            onChange={handleBetChange}
-          />
-          <span>배</span>
-        </div>
-        {/* 기록 */}
-        <div className="App-history">
-          <h2>승부기록</h2>
-          <p>{gameHistory.join(", ")}</p>
-        </div>
-      </div>
       <div>
-        <HandButton value="rock" onClick={handleButtonClick} />
-        <HandButton value="scissor" onClick={handleButtonClick} />
-        <HandButton value="paper" onClick={handleButtonClick} />
+        <h2 className="App-title">가위바위보</h2>
+        <img src={reset} />
+        <div className="score">
+          <h1 className="myScore">0&nbsp;:</h1>
+          <h1 className="otherScore">&nbsp;0</h1>
+        </div>
+        <div className="game-zone">
+          <img className="myGame" src={myGame} /> vs
+        </div>
+        <div className="point">
+          배점 <input type="number" />배
+        </div>
+        <div className="scoreHistory">승부기록</div>
+        <button className="rock">주먹</button>
+        <button className="sissor">가위</button>
+        <button className="paper">보</button>
       </div>
     </div>
   );
