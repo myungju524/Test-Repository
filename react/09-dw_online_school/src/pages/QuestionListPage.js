5c904a0be6e4c90cd391ee3bdc4bf29d280c25ec
mi <167054762+myungju524@users.noreply.github.com>
2024-07-19 15:59:54 +0900

    0303

--- react/09-dw_online_school/src/pages/QuestionListPage.js
+++ react/09-dw_online_school/src/pages/QuestionListPage.js
@@ -1,9 +1,33 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import styles from "./QuestionListPage.module.css";
 import ListPage from "../components/ListPage";
+import { getDatas } from "../api/firebase";
+import searchImg from "../assets/search.svg";
+let listItetms;
 
 function QuestionListPage(props) {
-  return <ListPage variant="community"></ListPage>;
+  const [items, setItems] = useState([]);
+
+  const handleLoad = async () => {
+    const resultData = await getDatas("questions");
+    listItetms = resultData;
+    console.log(resultData);
+    setItems(resultData);
+  };
+  useEffect(() => {
+    handleLoad();
+  }, []);
+  return (
+    <ListPage variant="community">
+      <form className={styles.form}>
+        <input placeholder="검색으로 질문 찾기" />
+        <button>
+          <img src={searchImg} />
+        </button>
+      </form>
+      <p className={styles.count}>총 {items.length}개 질문</p>
+    </ListPage>
+  );
 }
 
 export default QuestionListPage;
