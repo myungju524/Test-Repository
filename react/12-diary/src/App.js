b986215b0b58686f9ea009742f12aca83787d7d9
bsybsy01 <167054762+myungju524@users.noreply.github.com>
2024-07-31 17:16:28 +0900

    0101

--- react/12-diary/src/App.js
+++ react/12-diary/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import NewPage from "./pages/NewPage";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/">
             <Route index element={<HomePage />} />
-            {/* <Route path="new" />/ */}
+            <Route path="new" element={<NewPage />} />
             {/* <Route path="edit" /> */}
             {/* <Route path="diary" /> */}
           </Route>
--- react/12-diary/src/components/DiaryList.js
+++ react/12-diary/src/components/DiaryList.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "./Button";
 import "./DiaryList.css";
 import DiaryItem from "./DiaryItem";
+import { useNavigate } from "react-router-dom";
 
 const sortOptionList = [
   { name: "최신순", value: "latest" },
@@ -29,6 +30,7 @@ function ControlMenu({ optionList }) {
 }
 
 function DiaryList(props) {
+  const navigate = useNavigate();
   return (
     <div className="diaryList">
       <div className="menu_wrapper">
@@ -37,7 +39,11 @@ function DiaryList(props) {
           <ControlMenu optionList={filterOptionList} />
         </div>
         <div className="new_btn">
-          <Button text={"새 일기쓰기"} type="positive" />
+          <Button
+            text={"새 일기쓰기"}
+            type="positive"
+            onClick={() => navigate("/new")}
+          />
         </div>
       </div>
       <DiaryItem />
