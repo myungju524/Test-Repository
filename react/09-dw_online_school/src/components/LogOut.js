f1c250696f81d5a9d0b941658873681b11124af5
mj <167054762+myungju524@users.noreply.github.com>
2024-10-10 10:30:25 +0900

    00

--- react/09-dw_online_school/src/components/LogOut.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-function Logout(props) {
-  localStorage.removeItem("member");
-  return <Navigate to="/" />;
-}
-
-export default Logout;
